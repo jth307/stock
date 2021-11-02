5791231a6f130d021418cdb9c138bf990494e0b3
boo radley <booradley@boos-MacBook-Air.local>
2021-11-01 19:36:00 -0700

    styled demo button

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Landing from './components/Landing';
+import LoginForm from './components/LoginForm';
+import { BrowserRouter as Router, Route, Link, useHistory, Redirect } from 'react-router-dom';
 
 
-ReactDOM.render(<Landing />, document.getElementById('app'));
+
+
+ReactDOM.render(<Landing/>, document.getElementById('app'));
