a6c6c0f0cf1bfdd5d6d235ff9c6ba2de5e1d152c
boo radley <booradley@boos-MacBook-Air.local>
2021-11-12 20:09:36 -0800

    init

---
+++ server/helpers/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // CLOUD_API_TOKEN: 'pk_9aa140ae05fd445d8875978fd32685eb',
+  FINN_API_TOKEN: 'c5t3qhaad3icf7iiomug',
+  NEWS_API_TOKEN: '89ada765e31248c5b4658875ca682421',
+  CLOUD_API_TOKEN: 'pk_407d22f74e5d487cad48546e978435d1',
+
+};
+
+
